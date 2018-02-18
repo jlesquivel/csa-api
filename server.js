9be7e24f9cd4cb7d321556b1e7c5988b21a80141
jlesquivel <jlesquivel@hotmail.com>
2018-02-18 12:43:15 -0600

    actualizacion url de ssl

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function approveDomains(opts, certs, cb) {
     opts.domains = certs.altnames;
   }
   else {
-    opts.domains = ['csalib.ddns.net'];
+    opts.domains = ['csalib.ddns.net:8443'];
     opts.email = 'jlesquivel@hotmail.com';
     opts.agreeTos = true;
   }
@@ -25,7 +25,7 @@ var lex = require('greenlock-express').create({
 // handles acme-challenge and redirects to https
 require('http')
   .createServer(lex.middleware(require('redirect-https')()))
-  .listen(80, function () {
+  .listen(8080, function () {
     console.log('Listening for ACME http-01 challenges on', this.address());
   });
 
@@ -52,6 +52,6 @@ routes.configurar(app);
 // handles your app
 require('https')
   .createServer(lex.httpsOptions, lex.middleware(app))
-  .listen(443, function () {
+  .listen(8443, function () {
     console.log('Listening for ACME tls-sni-01 and serve app on', this.address());
   });
